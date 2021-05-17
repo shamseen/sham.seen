888b4e3ee3e154402bafeb8a9350b866450254f1
shamseen <shamseen.c@gmail.com>
2021-05-17 18:23:41 -0400

    basic sidenav and baackground img

--- src/main.js
+++ src/main.js
@@ -1,11 +1,42 @@
-import Sidebar from "./components/sidebar";
+import React, { useState } from 'react';
+import SkillSnapshot from './components/skillSnapshot';
+import { Button, Layout } from "antd";
+import {
+    IdcardFilled
+} from '@ant-design/icons';
+import 'antd/dist/antd.css';
 import "./styles/main.scss";
 
+const { Sider, Content } = Layout;
+
+
 export default function Main() {
+
+    const [open, setOpen] = useState(true);
+
     return (
-        <div className="main">
-            <Sidebar />
-            wassap
-        </div>
+        <Layout>
+
+            {/* ---- Quick overview of my skills ---- */}
+            <Sider trigger={null} collapsible collapsed={open}>
+                <SkillSnapshot />
+            </Sider>
+            {/* Toggle sider */}
+            <Button
+                icon={<IdcardFilled />}
+                onClick={() => { setOpen(!open) }}>
+            </Button>
+
+            <Content
+                className="site-layout-background content"
+                style={{
+                    margin: '24px 16px',
+                    padding: 24,
+                    minHeight: 280,
+                }}>
+                {/* TODO: Jumbotron, cards, FAB, etc */}
+                Content
+            </Content>
+        </Layout>
     )
 }
