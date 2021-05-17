657252edaea5ee9d8296fa0f332939efc7df096e
shamseen <shamseen.c@gmail.com>
2021-05-17 12:32:53 -0400

    boilerplate react stuffs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('JS linked');
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./main";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode >,
+    rootElement
+);
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./styles/main.scss";
+
+export default function Main() {
+    return (
+        <div className="main">
+            wassap
+        </div>
+    )
+}
