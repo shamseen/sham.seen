089c1bc6b1d5ca94f2d34d7b7e5f73e145c60b81
shamseen <shamseen.c@gmail.com>
2021-05-18 14:43:44 -0400

    skillSnapshot has avatar and boilerplate tabs

--- src/components/skillSnapshot.js
+++ src/components/skillSnapshot.js
@@ -1,22 +1,29 @@
-import { Menu } from "antd";
 import {
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
+    AntDesignOutlined,
+    CodeOutlined
 } from '@ant-design/icons';
+import { Avatar, Image, Space, Tabs } from "antd";
+const { TabPane } = Tabs;
+
 
 export default function SkillSnapshot() {
+
+
     return (
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-                nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-                nav 3
-            </Menu.Item>
-        </Menu>
+        <Space direction="vertical" id="skillSnapshot">
+            <Avatar
+                icon={<AntDesignOutlined />}
+                size={64}
+                alt="avatar image"
+            />
+            <Tabs centered defaultActiveKey="1" type="card" size='small'>
+                <TabPane tab={<CodeOutlined />} key="1" >
+                    Coding stats
+                </TabPane>
+                <TabPane tab="🧬" key="2" >
+                    Bio stats
+                </TabPane>
+            </Tabs>
+        </Space>
     )
 }
