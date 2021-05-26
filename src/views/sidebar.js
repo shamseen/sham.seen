bd50bdfb809b871661e765791814b784ddc2e9c8
shamseen <shamseen.c@gmail.com>
2021-05-26 15:00:46 -0400

    contact links + CSM badge #5

--- src/views/sidebar.js
+++ src/views/sidebar.js
@@ -1,22 +1,36 @@
 import WebDevSkills from "./skillSnapshots/webDev";
 import BiomedSkills from "./skillSnapshots/biomed";
-import { CodeOutlined } from '@ant-design/icons';
-import { Avatar, Space, Tabs, Typography } from "antd";
+import { CodeOutlined, GithubFilled, LinkedinFilled } from '@ant-design/icons';
+import { Avatar, Badge, Button, Space, Tabs, Typography } from "antd";
 const { TabPane } = Tabs;
-const { Title } = Typography;
+const { Link, Title } = Typography;
 import "../styles/sidebar.scss";
 
 
 export default function Sidebar() {
+    /* --- Content --- */
+    const contactLinks = [
+        {
+            name: 'github',
+            url: 'https://github.com/shamseen',
+            icon: <GithubFilled />
+        },
+        {
+            name: 'linkedin',
+            url: 'https://www.linkedin.com/in/shamseenr/',
+            icon: <LinkedinFilled />
+        }
+        // TODO: contact me btn
+    ]
 
     const skillTabs = [
         {
-            tab: <CodeOutlined />,
+            name: <CodeOutlined />,
             title: 'Experience',
             content: <WebDevSkills />
         },
         {
-            tab: '🧬',
+            name: '🧬',
             title: 'Experience',
             content: <BiomedSkills />
         }
@@ -24,27 +38,43 @@ export default function Sidebar() {
 
     return (
         <Space direction="vertical" id="sidebar" className="gray-bg">
-            <Avatar
-                size={100}
-                src="./moi.jpg"
-                alt="avatar image"
-            />
-            <Title level={3}>Shamseen Rahman</Title>
-            <div className="contact">
 
+            {/* Contact Info */}
+            <Badge count={"CSM®"} title="Certified ScrumMaster®" offset={[-5, 90]}>
+                <Avatar
+                    size={100}
+                    src="./moi.jpg"
+                    alt="avatar image"
+                />
+            </Badge>
+            <div id="contact">
+                <Title level={3} style={{ flexBasis: '100%' }}>Shamseen Rahman</Title>
+
+                {/* Icon links */}
+                {/* {contactLinks.map(link => {
+                    return <Link
+                        href={link.url} key={link.name}>{link.icon}</Link>;
+                })} */}
+                <Button.Group>
+                    {contactLinks.map(link => {
+                        return <Button size="large" type="link"
+                            href={link.url} key={link.name}
+                            icon={link.icon} />;
+                    })}
+                </Button.Group>
             </div>
+
+            {/* Skill snapshot */}
             <Tabs defaultActiveKey="0" type="card" size='large' className="skillSnapshot">
-                {
-                    skillTabs.map((tab, i) => {
-                        return (
-                            <TabPane tab={tab.tab} key={i}>
-                                <Title level={5}>{tab.title}</Title>
-                                {tab.content}
-                            </TabPane>
-                        );
-                    })
-                }
+                {skillTabs.map((tab, i) => {
+                    return (
+                        <TabPane tab={tab.name} key={i}>
+                            <Title level={5}>{tab.title}</Title>
+                            {tab.content}
+                        </TabPane>
+                    );
+                })}
             </Tabs>
-        </Space>
+        </Space >
     )
 }
