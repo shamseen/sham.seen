f1505e5c6150a506cceb18d648cabe337ba94d4a
shamseen <shamseen.c@gmail.com>
2021-05-25 20:49:28 -0400

    moved proj card to separate component

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,37 +1,45 @@
-import { useState } from 'react';
 import {
-  ArrowsAltOutlined,
-  GithubOutlined,
-  LinkOutlined
+  ArrowsAltOutlined, GithubOutlined, LinkOutlined
 } from '@ant-design/icons';
-import { Card, Divider, Skeleton, Space } from 'antd';
+import { Card, Divider, Popover, Skeleton, Space, Typography } from 'antd';
+const { Link, Paragraph } = Typography;
 const { Meta } = Card;
 
 export default function ProjectCard({ proj }) {
-  const [actions, setActions] = useState([]);
 
-  const showActions = (isHovered) => {
-    setActions(isHovered ?
-      [
-        <ArrowsAltOutlined key="ellipsis" />,
-        <LinkOutlined key="ellipsis" />,
-        <GithubOutlined key="ellipsis" />,
-      ]
-
-      : []
-    );
+  const ellipses =
+  {
+    rows: 2,
+    expandable: true,
+    symbol: 'more',
   }
+
   return (
-    <Card
-      bordered={false}
-      className="project"
-      hoverable
-      cover={<Skeleton.Image />}
-      actions={actions}
-      onMouseOver={() => showActions(true)}
-      onMouseOut={() => showActions(false)}
-    >
-      <Meta title={proj.title} description={proj.desc}></Meta>
-    </Card>
+    /* --- Hovering over card will show links to site & repo -- */
+    <Popover content={
+      [
+        <Link href={proj.url} ><LinkOutlined key="ellipsis" /></Link>,
+        <Divider type="vertical" />,
+        <Link href={proj.repo}><GithubOutlined key="ellipsis" /></Link>
+      ]
+    }>
+
+      <Card
+        bordered={false}
+        className="project"
+        key={proj.title}
+        hoverable
+        cover={<Skeleton.Image />}
+      >
+        <Meta title={proj.title}
+          description={
+            <Paragraph ellipsis={ellipses}>
+              {proj.desc}
+            </Paragraph>
+          }
+        >
+        </Meta>
+      </Card>
+    </Popover>
   )
 }
--- src/components/projectDeck.js
+++ src/components/projectDeck.js
@@ -1,15 +1,11 @@
-import {
-  ArrowsAltOutlined, GithubOutlined, LinkOutlined
-} from '@ant-design/icons';
-import { Button, Card, Divider, Popover, Skeleton, Space, Typography } from 'antd';
-const { Link, Paragraph } = Typography;
-const { Meta } = Card;
+import { Space } from 'antd';
+import ProjectCard from './projectCard';
 
 const placeholder = new Array(3).fill(
   {
     title: "Project Title",
     image: '',
-    desc: 'Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team. Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    desc: 'Nunc eu pharetra metus. Vivamus semper aliquam eros, eget vestibulum nisi consequat ac. Sed consequat viverra purus. Aliquam id blandit quam, at venenatis nulla. Praesent fringilla ultricies velit egestas tempus. Etiam luctus tortor faucibus pretium tempor. Proin non condimentum nunc. In varius, erat a blandit pellentesque, lorem nibh blandit turpis, eu fringilla nulla mi vel tortor. Vivamus egestas odio purus, non egestas nisl scelerisque eget. Nam nec metus malesuada, sodales ante quis, mollis velit.',
     url: '',
     repo: ''
   }
@@ -19,8 +15,6 @@ export default function ProjectDeck({ projects = placeholder }) {
   // Desktop: row of cards
   // Mobile: carousel of cards
 
-
-
   return (
     <Space
       className="projectDeck"
@@ -28,40 +22,10 @@ export default function ProjectDeck({ projects = placeholder }) {
       align="center"
     >
 
+      {/* TO DO: carousel of cards on mobile */}
       {
         projects.map(p => {
-          return (
-            /* --- Hovering over card will show links to site & repo -- */
-            <Popover content={
-              [
-                <Link href={p.url} ><LinkOutlined key="ellipsis" /></Link>,
-                <Divider type="vertical" />,
-                <Link href={p.repo}><GithubOutlined key="ellipsis" /></Link>
-              ]
-            }>
-
-              <Card
-                bordered={false}
-                className="project"
-                key={p.title}
-                hoverable
-                cover={<Skeleton.Image />}
-              >
-                <Meta title={p.title}
-                  description={
-                    <Paragraph ellipsis={{
-                      rows: 2,
-                      expandable: true,
-                      symbol: 'more',
-                    }}>
-                      {p.desc}
-                    </Paragraph>}
-                >
-
-                </Meta>
-              </Card>
-            </Popover>
-          );
+          return <ProjectCard proj={p} key={p.title} />;
         })
       }
     </Space>
