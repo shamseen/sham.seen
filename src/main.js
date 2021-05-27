317cfc2f3436fdd9fb8fde937253d463f3aca8ae
shamseen <shamseen.c@gmail.com>
2021-05-27 14:09:57 -0400

    antd sider with breakpoint, defaults closed #8

--- src/main.js
+++ src/main.js
@@ -9,28 +9,35 @@ const { Sider, Content } = Layout;
 
 
 export default function Main() {
+    const defaultWidth = '250px';
+    const [collapsed, setCollapsed] = useState(false);
+    const [siderWidth, setWidth] = useState(defaultWidth);
 
-    const [open, setOpen] = useState(true);
+    const showFullSidebar = (isSmallScreen) => {
+        console.log("breakpt. small screen: ", isSmallScreen);
 
+        // on mobile, sidebar should be full width
+        setCollapsed(!isSmallScreen);
+        setWidth(isSmallScreen ? '80%' : defaultWidth);
+    }
     return (
         <Layout id="layout">
 
             {/* ---- Quick overview of my skills ---- */}
             {/* TODO: responsive */}
             <Sider
-                width="30%"
                 theme="light"
+                width={siderWidth}
                 collapsible={true}
-                collapsed={!open}
+                collapsed={collapsed}
                 collapsedWidth="0"
-                // breakpoint="sm"
-                // onBreakpoint={broken => {
-                //     console.log("broken: ", broken);
-                // }}
-                onCollapse={(collapsed, type) => {
-                    setOpen(!collapsed);
+                breakpoint="sm"
+                onBreakpoint={isSmallScreen => { showFullSidebar(isSmallScreen) }}
+                onCollapse={(wasCollapsed, type) => {
+                    setCollapsed(wasCollapsed);
+                    console.log(wasCollapsed, type);
                 }}
-                className="gray-bg" >
+            >
                 <Sidebar />
             </Sider>
 
