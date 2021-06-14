3c76d668099a76c6e3f967732f6b2200d99e7249
shamseen <shamseen.c@gmail.com>
2021-06-14 17:50:23 -0400

    sorted projects by stack #28

--- src/dataServices/slateHostDataService.js
+++ src/dataServices/slateHostDataService.js
@@ -3,35 +3,53 @@ import { BranchesOutlined } from "@ant-design/icons";
 const baseUrl = 'https://slate.host/api/v2/get';
 const fileUrl = 'https://slate.textile.io/ipfs/';
 
+const projects = {
+  backend: [],
+  frontend: [],
+  fullstack: [],
+};
 const images = {
-  url: fileUrl,
-  projects: [],
+  projects: {},
   resume: null,
   sidebar: [],
+  url: fileUrl,
 }
 
 const sortFiles = (json) => {
 
   json.user.library.map((item, i) => {
-    switch (item.data.author) {
+    const name = item.data.author.split(' '); // comes in as 'frontend Ludo'
 
-      case 'projects': images.projects.push(item); break;
+    item.data.author = name[1]  // removing the 'frontend' part
+
+    switch (name[0]) {
 
       case 'resume': images.resume = item; break;
 
       case 'sidebar': images.sidebar.push(item); break;
 
+      // project images
+      case 'backend': projects.backend.push(item); break;
+      case 'frontend': projects.frontend.push(item); break;
+      case 'fullstack': projects.fullstack.push(item); break;
+
       default: break;
     }
   });
+
+  console.log(json);
+
+  images.projects = projects;
+  console.log(projects);
+  console.log(images.projects);
   // console.log(images);
   // console.log(JSON.stringify(images, null, 2));
+
   return images;
 }
 
 export const getFiles = async () => {
   const auth = `Basic ${process.env.REACT_APP_SLATEHOST_KEY}`;
-  console.log(auth);
 
   try {
     const response = await fetch(baseUrl, {
